cb123222c468bf9d8a3bbc7f68eeed8c500a2013
Prit210 <pritjagani724@gmail.com>
2024-10-15 11:15:11 +0530

    Added more attributes and methods to course class.

--- course.java
+++ course.java
@@ -2,8 +2,22 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Course {
+public class course {
+
+    String courseID;
+    String title;
+    int credits;
+    ArrayList<String> preRequList;
+    ArrayList<String> professors;
+    ArrayList<String> enrolledStudents;
+    int studentCapacity;
+    LocalDate dropDeadline;
+
+
     public static boolean courseExists(String courseCode) {
         try (Connection connection = DatabaseConnection.getConnection()) {
             String query = "SELECT * FROM courses WHERE course_code = ?";
@@ -15,5 +29,83 @@ public class Course {
             e.printStackTrace();
             return false;
         }
+    }
+
+    public boolean  isFull(){
+        if((enrolledStudents.size())<studentCapacity){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    public int availableSeats(){
+        return (studentCapacity-(enrolledStudents.size()));
+    }
+
+    public boolean  isDeadlinePassed(){
+        return dropDeadline.isBefore(LocalDate.now());
+    }
+
+    public void getData(){
+        System.out.println("Here is the data about the course :");
+
+        System.out.println("Course ID: "+courseID);
+        System.out.println("Course Title: "+title);
+        System.out.println("Total credits of the course:"+credits);
+        System.out.println("Prerequisite for the course: "+preRequList);
+        System.out.println("Student capacity: "+studentCapacity);
+        System.out.println("Number of student enrolled: "+enrolledStudents.size());
+        System.out.println("professors theaching the course: "+professors);
+        System.out.println("List of all the students: "+enrolledStudents);
+        System.out.println("Deadlinee to drop the course:"+dropDeadline);
+
+    }
+
+    public String getSpecificData(){
+        System.out.println("What specific data do you want ?");
+        System.out.println("\n 1.Course ID");
+        System.out.println("\n 2.Course title");
+        System.out.println("\n 3.Prerequisite to enroll in course");
+        System.out.println("\n 4.Credits");
+        System.out.println("\n 5.Professors teaching the course");
+        System.out.println("\n 6.Student capacity of the course");
+        System.out.println("\n 7.Students learnng the course");
+        System.out.println("\n 8.Course drop deadline");
+
+        Scanner sc=new Scanner(System.in);
+
+        int choice = sc.nextInt();
+
+        switch (choice) {
+            case 1:
+                return courseID;   
+            
+            case 2:
+                return title;
+
+            case 3:
+                return preRequList.toString();
+    
+            case 4:
+                return ""+credits;
+
+            case 5:
+                return professors.toString();
+
+            case 6:
+                return ""+studentCapacity;
+
+            case 7:
+                return enrolledStudents.toString();
+
+            case 8:
+                return ""+dropDeadline;
+
+            default:
+                return "Invalid input. Please try again.";
+        }
+
     }
 }
